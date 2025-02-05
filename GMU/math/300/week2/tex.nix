6cbb317ed87e7eec3ab751875b646fa949213634
Andrew Jones <aljones15@gmail.com>
2025-02-05 15:26:12 -0500

    Add initial tex.nix & tex file.

--- GMU/math/300/week2/tex.nix
+++ GMU/math/300/week2/tex.nix
@@ -2,8 +2,12 @@ with import <nixpkgs> {  };
 {
   texEnv = stdenv.mkDerivation rec {
     name="LaTeX Environment";
+    tex = (texlive.combine {
+      inherit (pkgs.texlive) scheme-basic;
+      inherit (pkgs.texlivePackages) titlesec footmisc greek-fontenc;
+    });
     buildInputs = [
-      texlive.combined.scheme-basic
+      tex
     ];
   };
 }
