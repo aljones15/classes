5a2eb49642fb98b04da8b40d6664e13ccc3ede8f
Andrew Jones <aljones15@gmail.com>
2025-02-05 20:08:37 -0500

    Use scheme-full & finish page 2.

--- GMU/math/300/week2/tex.nix
+++ GMU/math/300/week2/tex.nix
@@ -3,7 +3,7 @@ with import <nixpkgs> {  };
   texEnv = stdenv.mkDerivation rec {
     name="LaTeX Environment";
     tex = (texlive.combine {
-      inherit (pkgs.texlive) scheme-basic;
+      inherit (pkgs.texlive) scheme-full;
       inherit (pkgs.texlivePackages) titlesec footmisc greek-fontenc;
     });
     buildInputs = [
